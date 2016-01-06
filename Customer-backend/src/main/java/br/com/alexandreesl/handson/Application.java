1a27b4eee7c4c872c40fe3dd104b9e846cd714ee
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2016-01-06 20:00:47 -0200

    implementation changed to RestController and Eureka registration/discovery

--- Customer-backend/src/main/java/br/com/alexandreesl/handson/Application.java
+++ Customer-backend/src/main/java/br/com/alexandreesl/handson/Application.java
@@ -1,9 +1,15 @@
 package br.com.alexandreesl.handson;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableEurekaClient
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
